fd317902929c694cba4fef087cd02ce876dfc2b6
sunteya <sunteya@gmail.com>
2014-11-11 15:30:35 +0800

    upgrade rspec to 3.1

--- spec/fume/cancan/controller_extensions_spec.rb
+++ spec/fume/cancan/controller_extensions_spec.rb
@@ -1,6 +1,6 @@
-require "spec_helper"
+require "rails_helper"
 
-describe Fume::Cancan::ControllerExtensions, type: :controller do
+RSpec.describe Fume::Cancan::ControllerExtensions, type: :controller do
   describe "#authorize_object" do
     controller(ApplicationController) {}
     action { get :index }
---
+++ spec/rails_helper.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+require "simplecov"
+require 'coveralls'
+Coveralls.wear!
+# SimpleCov.start
+
+require "pry-nav"
+require 'combustion'
+Combustion.initialize! :action_controller
+require "rspec/rails"
+require "rspec-do_action"
+
+
+RSpec.configure do |config|
+  config.infer_base_class_for_anonymous_controllers = false
+end
